928b5df40dd211596df8cb9f733b00b4e9e95a53
roxanatic <37555036+roxanatic@users.noreply.github.com>
2018-03-19 22:25:38 +0200

    RollingBall

---
+++ New Unity Project/Assets/Scripts/DoNote.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNote : MonoBehaviour
+{
+    public AudioClip impact;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "LeftHand")
+        {
+            audioSource.PlayOneShot(impact, 0.7F);
+        }
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+
+    public float speed;
+    AudioSource source;
+    AudioClip sound;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+       
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Cube")
+        {
+            col.gameObject.GetComponent<Renderer>().material.color = Color.black;
+           
+        }
+        else if (col.gameObject.name == "Cube (2)")
+        {
+            col.gameObject.GetComponent<Renderer>().material.color = Color.grey;
+
+        }
+        else if (col.gameObject.name == "Cube (3)")
+        {
+            col.gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+        }
+        else if (col.gameObject.name == "Cube (4)")
+        {
+            col.gameObject.GetComponent<Renderer>().material.color = Color.green;
+
+        }
+        else if (col.gameObject.name == "Cube (5)")
+        {
+            col.gameObject.GetComponent<Renderer>().material.color = Color.magenta;
+
+        }
+        else if (col.gameObject.name == "Cube (6)")
+        {
+            col.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+
+        }
+        else if (col.gameObject.name == "Cube (7)")
+        {
+            col.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+
+        }
+
+
+    }
+}
